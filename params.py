d3c7edd9729b35c08e935e62a41d80485ce9b9a1
Noah Chalifour <noahchalifour@Noahs-MacBook-Pro-3.local>
2018-01-28 15:01:39 -0500

    Fixed params

--- params.py
+++ params.py
@@ -1,17 +1,23 @@
 from datetime import datetime
 
 params = {
-    'symbols': ['BTC-USD', 'ETH-USD', 'XRP-USD', 'BCH-USD', 'ADA-USD'],
+    'symbols': [
+        'BTC-USD', 
+        'BCH-USD', 
+        'ETH-USD', 
+        'LTC-USD'
+    ],
     'start_date': datetime(2000, 1, 1).date(),
     'end_date': datetime.today(),
     'chunk': 10,
     'num_hidden_layers': 3,
-    'lstm_units': 100,
+    'lstm_units': 512,
     'loss': 'mean_squared_error',
     'optimizer': 'adam',
-    'epochs': 1,
-    'batch_size': 1,
+    'epochs': None,
+    'batch_size': 64,
+    'steps_per_checkpoint': 5,
     'test_size': 0.2,
-    'max_data': 50, # None to use all data
+    'max_data': None,
     'model_type': 'regression'
 }
